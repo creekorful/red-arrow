c0877ed0df4d1b33778459bbceb6c4b285238a55
Aloïs Micard <lunamicard@gmail.com>
2018-12-29 13:24:08 +0100

    Create base application class

---
+++ Source/Game/Application.h
@@ -0,0 +1,16 @@
+#ifndef RED_ARROW_APPLICATION_H
+#define RED_ARROW_APPLICATION_H
+
+/**
+ * Represent the game
+ */
+class Application
+{
+public:
+    Application(int argc, char** argv);
+
+    int execute();
+};
+
+
+#endif //RED_ARROW_APPLICATION_H
---
+++ Source/main.cpp
@@ -0,0 +1,8 @@
+
+#include "Game/Application.h"
+
+int main(int argc, char** argv)
+{
+    auto application = new Application(argc, argv);
+    return application->execute();
+}
